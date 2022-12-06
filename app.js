84198b841486e6c981566e66ddc0bc5b2c671557
Muhammad Dimas Adijanto <adijanto.205026@mhs.its.ac.id>
2022-12-06 08:39:58 +0700

    init

---
+++ app.js
@@ -0,0 +1,9 @@
+const cpuInformation = process.memoryUsage();
+
+console.log(cpuInformation);
+
+
+const firstName = process.argv[2];
+const lastName = process.argv[3];
+
+console.log(`Hello ${firstName} ${lastName}`);
