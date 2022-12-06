42b7ad1a43a64a17f2b5d67ff4153ad440a903d4
Muhammad Dimas Adijanto <adijanto.205026@mhs.its.ac.id>
2022-12-06 14:50:07 +0700

    filesystem and readable stream

---
+++ filesystem.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+const fileReadCallback = (error, data) => {
+    if (error) {
+        console.log('Gagal membaca berkas');
+        return;
+    }
+    console.log(data);
+}
+
+fs.readFile('todo.txt', 'UTF-8', fileReadCallback);
